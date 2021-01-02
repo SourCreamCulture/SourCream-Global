ebeb216240abf872bdb790884cde3366bb235363
Dallin Bland <73759064+SourCreamCulture@users.noreply.github.com>
2021-01-02 18:37:26 -0500

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,9 +1,9 @@
-module.exports = async (client, message) => {
+module.exports = async (bot, message) => {
   if (message.author.bot) return;
 
   //Prefixes also have mention match
-  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-  const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
+  const prefixMention = new RegExp(`^<@!?${bot.user.id}> `);
+  const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : bot.config.prefix;
 
   if (message.content.indexOf(prefix) !== 0) return;
 
@@ -12,11 +12,11 @@ module.exports = async (client, message) => {
   const command = args.shift().toLowerCase();
 
   //Searching a command
-  const cmd = client.commands.get(command);
+  const cmd = bot.commands.get(command);
   //Searching a command aliases
-  const aliases = client.commands.find(x => x.info.aliases.includes(command))
+  //const aliases = client.commands.find(x => x.info.aliases.includes(command))
 
-  //if(message.channel.type === "dm")return message.channel.send("None of the commands work in DMs. So please use commands in server!")
+  //if(message.channel.type === "dm")return message.reply("None of the commands work in DMs. So please use commands in server!")
 process.on("unhandledRejection", (reason, promise) => {
     try {
         console.error("Unhandled Rejection at: ", promise, "reason: ", reason.stack || reason);
@@ -29,8 +29,8 @@ require('events').EventEmitter.defaultMaxListeners = 25
 
   //Executing the codes when we get the command or aliases
   if(cmd){
-    cmd.run(client, message, args);
-  }else if(aliases){
-    aliases.run(client, message, args);
-  }else return
+    cmd.run(bot, message, args);
+  }else return //if(aliases){
+    //aliases.run(client, message, args);
+  //}else return
 };
